919bfcc0e55c224900538d47f67dcb8b2d601be7
Akita20 <jamila.osmn@gmail.com>
2020-11-10 09:49:58 -0800

    New changed and updated

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ public class Main {
         System.out.println("Hello");
         // B21 is good at intelliJ
         //mac user and windows user are happy to see commit dialog
-
+     // I have already published on Github.com
+        // but do I need to share again?
+        //absultley not.
 
 
 
